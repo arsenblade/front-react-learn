3e5ccaa59c1fc33330df9e2a2d37b6483a7204eb
Arsen <arsenblade@mail.ru>
2023-01-11 01:31:34 +0500

    Изменения для деплоя

--- src/constants/serverPath.ts
+++ src/constants/serverPath.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8000'
+export const API_URL = 'https://js-front-learn.onrender.com'
 
 export const getLoginUrl = () => '/login'
 export const getRegisterUrl = () => '/users'
